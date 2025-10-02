4acaae8bc114d29bb2e84845b7052bbfb22d924d
clovis stefanutti <c.stefanutti@outlook.fr>
2025-10-02 17:38:08 +0200

    feat/1-desktop-menu-header-finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logoHeader from "../assets/logos/logo-header.png";
 
 const Header = () => {
   return (
@@ -6,7 +7,7 @@ const Header = () => {
       <div className="max-w-6xl mx-auto flex justify-between items-center py-6 px-4">
         <div className="flex items-center gap-4">
           <img
-            src="/logo.png"
+            src={logoHeader}
             alt="Logo Pizzeria"
             className="w-16 h-16 object-contain"
           />
@@ -15,20 +16,17 @@ const Header = () => {
           </h1>
         </div>
 
-        <nav className="flex gap-10 text-black text-lg">
-          <a href="#about" className="hover:text-yellow-500 transition-colors">
+        <nav className="flex gap-10 text-black font-semibold text-lg">
+          <a href="#about" className="hover:text-gray-400 transition-colors">
             About us
           </a>
-          <a href="#menu" className="hover:text-yellow-500 transition-colors">
+          <a href="#menu" className="hover:text-gray-400 transition-colors">
             Menu
           </a>
-          <a href="#find" className="hover:text-yellow-500 transition-colors">
+          <a href="#find" className="hover:text-gray-400 transition-colors">
             Find us
           </a>
-          <a
-            href="#contact"
-            className="hover:text-yellow-500 transition-colors"
-          >
+          <a href="#contact" className="hover:text-gray-400 transition-colors">
             Contact
           </a>
         </nav>
