a0488939c5a5d61a123c5efa2d7db9d26c0d83c7
clovis stefanutti <c.stefanutti@outlook.fr>
2025-10-02 20:45:17 +0200

    feat/5-menu-section-finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
+import Menu from "./components/Menu";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <AboutUs />
+      <Menu />
     </>
   );
 }
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,15 +6,17 @@ const AboutUs = () => {
     <section className="w-full h-[559px] bg-[#D9D9D9] flex items-center justify-center px-4 py-12">
       <div className="flex flex-col md:flex-row items-center md:items-start gap-12 mx-auto">
         <div className="hidden md:block flex-shrink-0">
-          <img
-            src={aboutImage}
-            alt="About us"
-            className="w-[331px] h-[395px] object-cover rounded-lg"
-          />
+          <div className="w-[331px] h-[395px] rounded-lg shadow-[0_10px_25px_rgba(0,0,0,0.5)]">
+            <img
+              src={aboutImage}
+              alt="About us"
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </div>
         </div>
 
         <div className="flex flex-col justify-center h-[395px] gap-6 text-gray-700">
-          <h2 className="uppercase font-bold text-gray-700 text-3xl pb-1">
+          <h2 className="uppercase font-bold text-gray-700 text-3xl leading-none">
             <span className="inline-block border-b-[1px] border-gray-700">
               About Us
             </span>
