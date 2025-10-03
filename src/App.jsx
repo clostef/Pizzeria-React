632dbd4475ecfd7ae2628166c139ef6fb5eb3100
clovis stefanutti <c.stefanutti@outlook.fr>
2025-10-03 23:03:37 +0200

    feat/7-find-us-finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Menu from "./components/Menu";
 import FindUs from "./components/FindUs";
+import Footer from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <AboutUs />
       <Menu />
       <FindUs />
+      <Footer />
     </>
   );
 }
